a95c4205e6b0fcda145183d32b844f7829896220
unknown <dariop1111@gmail.com>
2021-10-04 17:45:33 +0200

    first commit

---
+++ assets/js/opg.js
@@ -0,0 +1,14 @@
+new Splide( '.slider1-wrap',{
+    type:"slide",
+    rewind: true,
+    autoplay: true,
+    interval: 4000,
+} ).mount();
+new Splide( '.slider2-wrap',{
+    type:"loop",
+    perPage:3,
+    autoplay: true,
+    interval: 4000,
+} ).mount();
+
+
